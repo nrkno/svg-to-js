5ca03fac14145db80203073a09db6cb9556198b8
Eirik Backer <eirik.backer@gmail.com>
2017-09-15 20:31:12 +0200

    Change injection mechanism to aviod adblockers stopping icon rendering (#2)

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-sync*/
 const path = require('path');
 const fs = require('fs');
 
@@ -12,7 +11,7 @@ module.exports = (config) => Promise
   .resolve(fs.readdirSync(config.srcPath).filter((file) => file.slice(-4) === '.svg'))
   .then((files) => files.map((file) => svgToSymbol(file, fs.readFileSync(path.join(config.srcPath, file)))))
   .then((symbols) => `<svg xmlns="http://www.w3.org/2000/svg" style="display:none">${symbols.join('')}</svg>`)
-  .then((svg) => `document.documentElement.firstElementChild.insertAdjacentHTML('beforeend', '${svg}');`)
+  .then((svg) => `(function(el){el.innerHTML='${svg}';document.documentElement.lastElementChild.appendChild(el.firstElementChild)})(document.createElement('div'))`)
   .then((js) => `/*!${config.banner}*/\n${js}`)
   .then((js) => {
     fs.writeFileSync(path.join(config.distPath, config.svgFileName), js);

e75c956180f8894c33c4199c878d0c94d37a1385
Eirik Backer <eirik.backer@gmail.com>
2019-03-20 13:37:19 +0100

    Upgrade to support more file formats (#7)

--- README.md
+++ README.md
@@ -4,19 +4,27 @@ Module for concatenating SVG files into JavaScript.
 ##### Why load icons as JavaScript?
 SVG symbols are great for styling and accessibility, but can not load cross domain, or from external file and in IE (9,10,11). Javascript provides us a cacheable, cross-domain method load the icons, without adding extra overhead to each html-file.
 
-
 ## Usage
 ```
 const svgtojs = require('@nrk/svg-to-js')
 const options = {
-  svgFileName: 'core-icons.js',                 // Name of js-widh-svgs-file to create
-  svgFileNameMin: 'core-icons.min.js',          // Name of minified js-widh-svgs-file
-  srcPath: path.join(__dirname, '../src'),      // Folder of original svg files
-  distPath: path.join(__dirname, '../dist'),    // Where to put js-widh-svgs-file
-  banner: 'Copyright 2015-2017'                 // Text to add to top of file
+  input: 'src/'               // Required. Folder with SVG files
+  banner: 'Copyright NRK',    // Text to add to top of file
+
+  // svgtojs always returns Object of outputs,
+  // but can optionally also write files:
+  esm: 'core-icons.esm.js',   // ES module for bundlers exposing `export const iconName = '<svg...'`
+  cjs: 'core-icons.js',       // CommonJS for Node exposing `module.exports = { iconName: '<svg...' }`
+  esmx 'core-icons.esm.jsx',  // JSX ES module, exposing React components with `export`
+  cjsx: 'core-icons.cjs.jsx', // JSX CommonJS, exposing React components with `module.exports`
+  iife: 'core-icons.min.js'   // Self executing <script>, exposing all icons as symbols on page
 }
 
-svgtojs(options)
-  .then(() => console.log('Merged and minified SVG'))
-  .catch((err) => console.log(err.stack))
+svgtojs(options) // => Returns {
+  esm: String,
+  cjs: String,
+  esmx: String,
+  cjsx: String,
+  iife: String
+}
 ```
--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const path = require('path')
 const fs = require('fs')
 
-const svgToSymbol = (file, xml) => String(xml) // Work with file as string
-  .replace('>', ` id="${file.slice(0, -4)}">`) // Add filename as id
-  .replace(/(<\/?)svg/gi, '$1symbol') // Convert to symbols
-  .replace(/\s*xmlns=[^\s>]+/gi, '') // Remove xmlns
-  .replace(/\s*([<>])\s*/g, '$1') // Strip white space around tokens
+module.exports = function (config) {
+  const banner = config.banner || 'svg-to-js'
+  const scale = config.scale || 1
 
-module.exports = (config) => Promise
-  .resolve(fs.readdirSync(config.srcPath).filter((file) => file.slice(-4) === '.svg'))
-  .then((files) => files.map((file) => svgToSymbol(file, fs.readFileSync(path.join(config.srcPath, file)))))
-  .then((symbols) => `<svg xmlns="http://www.w3.org/2000/svg" style="display:none">${symbols.join('')}</svg>`)
-  .then((svg) => `(function(el){el.innerHTML='${svg}';document.documentElement.lastElementChild.appendChild(el.firstElementChild)})(document.createElement('div'))`)
-  .then((js) => `/*!${config.banner}*/\n${js}`)
-  .then((js) => {
-    fs.writeFileSync(path.join(config.distPath, config.svgFileName), js)
-    fs.writeFileSync(path.join(config.distPath, config.svgFileNameMin), js)
-    return js
+  const icons = fs.readdirSync(config.input)
+    .filter((file) => file.slice(-4) === '.svg')
+    .map((file) => {
+      const code = String(fs.readFileSync(path.join(config.input, file)))
+      const size = String(code.match(/viewBox="[^"]+/)).slice(9)
+
+      const name = file.slice(0, -4)
+      const body = code.replace(/^[^>]+>|<[^<]+$/g, '').replace(/\s*([<>])\s*/g, '$1') // Minified SVG body
+      const camelCase = name.replace(/-+./g, (m) => m.slice(-1).toUpperCase())
+      const titleCase = camelCase.replace(/./, (m) => m.toUpperCase())
+      const [w, h] = size.split(' ').slice(2).map((val) => `${(val / scale).toFixed(3)}em`)
+
+      if (!h) throw new Error(`Malformed viewBox in SVG ${file}`)
+
+      return {
+        camelCase,
+        titleCase,
+        symbol: `<symbol viewBox="${size}" id="${name}">${body}</symbol>`,
+        svg: `<svg viewBox="${size}" width="${w}" height="${h}" aria-hidden="true" focusable="false">${body}</svg>`,
+        jsx: `return React.createElement('svg', {'aria-hidden': true, width: '${w}', height: '${h}', viewBox: '${size}', dangerouslySetInnerHTML: {__html: '${body}'}})`
+      }
+    })
+
+  const result = {
+    iife: `/*!${banner}*/\n(function(el){el.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" style="display:none">${icons.map(({ symbol }) => symbol).join('')}</svg>';document.head.appendChild(el.firstElementChild)})(document.createElement('div'))`,
+    cjs: icons.map(({ camelCase, svg }) => `exports.${camelCase} = '${svg}'`).join('\n'),
+    esm: icons.map(({ camelCase, svg }) => `export const ${camelCase} = '${svg}'`).join('\n'),
+    cjsx: `const React = require('react')${icons.map(({ titleCase, jsx }) => `\nexports.${titleCase} = function () {${jsx}}`).join('')}`,
+    esmx: `import React from 'react'${icons.map(({ titleCase, jsx }) => `\nexport function ${titleCase} () {${jsx}}`).join('')}`
+  }
+
+  // Save files if specified in config
+  Object.keys(config).forEach((type) => {
+    if (result[type]) fs.writeFileSync(config[type], result[type])
   })
+
+  return result
+}
